65cd0e1152caece79b62893acd6dc9a84b384c0d
Andreas Stamos <stamos.aa@gmail.com>
2023-06-03 02:48:05 +0300

    Filter Borrows/Bookings in Lib Editor using First Name/Last Name

--- client/src/pages/lib_editor/Borrows.js
+++ client/src/pages/lib_editor/Borrows.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { CircularProgress } from '@mui/material';
+import { CircularProgress, TextField, Box } from '@mui/material';
 import BorrowsTable from '../../Components/BorrowsTable';
+import debounce from "lodash/debounce";
 
 function Borrows() {
 
@@ -9,37 +10,55 @@ function Borrows() {
   const [borrows, setBorrows] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  async function fetchBorrows() {
-    const response = await axios.post('http://localhost:5000/lib-api/get-borrows/', {}, {
-      headers: {
-         'Access-Control-Expose-Headers' : '*',
-         'Access-Control-Allow-Origin': '*', 
-         'Content-Type': 'application/json',
-         'Authorization': `Bearer ${localStorage.getItem('authTokens')}`,
-      }
-  });
-  setBorrows(response?.data?.borrows);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  console.log(response?.data?.borrows);
-  setLoading(false);
+  async function fetchBorrows() {
+    const payload = {
+      ...(firstName?.length > 0 && {first_name: firstName}),
+      ...(lastName?.length > 0 && {last_name: lastName}),
+    }
+
+    const response = await axios.post('http://localhost:5000/lib-api/get-borrows/', payload, {
+      headers: {
+        'Access-Control-Expose-Headers' : '*',
+        'Access-Control-Allow-Origin': '*', 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('authTokens')}`,
+      }
+    });
+    setBorrows(response?.data?.borrows);
+
+    setLoading(false);
   }
 
   useEffect(() => {
-    fetchBorrows();
-  }, [])
+    const debounced_fetchBorrows = debounce(() => {
+      setLoading(true);
+      fetchBorrows();
+    }, 500); //500ms between search calls to api.
+    debounced_fetchBorrows();
+
+    return () => {debounced_fetchBorrows.cancel();}
+  }, [firstName, lastName]);
+
 
   return (
     <div className='dashboard-component'>
-        <h2 className='component-title'>Δανεισμοί</h2>
-        <div className='component-details'>
-          {loading && <CircularProgress />}
-          {borrows && (borrows.length > 0 ? <BorrowsTable 
+      <h2 className='component-title'>Δανεισμοί</h2>
+      <Box sx={{display: 'flex', flexDirection: 'column', rowGap: '1rem'}}>
+        <Box sx={{display:'flex', columnGap: '1rem', ml: 'auto'}}>
+          <TextField value={firstName} label="Όνομα" onChange={(e) => {setFirstName(e.target.value);}} />
+          <TextField value={lastName} label="Επώνυμο" onChange={(e) => {setLastName(e.target.value);}} />
+        </Box>
+        {loading && <CircularProgress />}
+        {borrows && (borrows.length > 0 ? <BorrowsTable 
           data={borrows} fetchBorrows={fetchBorrows}
-          /> : <h3>Δεν βρέθηκαν δανεισμοί.</h3>)}
-        </div>
-        
-    
-</div>
+        /> : <h3>Δεν βρέθηκαν δανεισμοί.</h3>)}
+      </Box>
+
+
+    </div>
   )
 }
 
